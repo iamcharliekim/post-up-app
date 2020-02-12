1774be6c603b54ed32a8faae7de2ea98457b44ad
Charlie Kim <cwkim3@gmail.com>
2020-02-11 21:58:24 -0500

    updated env variables for deploy

--- src/Services/TokenService.js
+++ src/Services/TokenService.js
@@ -2,15 +2,15 @@ import config from '../config'
 
 const TokenService = {
     saveAuthToken(token) {
-        window.sessionStorage.setItem(config.TOKEN_KEY, token)
+        window.sessionStorage.setItem(config.REACT_APP_TOKEN_KEY, token)
     },    
 
     getAuthToken() {
-        return window.sessionStorage.getItem(config.TOKEN_KEY)
+        return window.sessionStorage.getItem(config.REACT_APP_TOKEN_KEY)
     },
 
     clearAuthToken() {
-        window.sessionStorage.removeItem(config.TOKEN_KEY)
+        window.sessionStorage.removeItem(config.REACT_APP_TOKEN_KEY)
     },
 
     hasAuthToken() {
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://sleepy-fjord-75293.herokuapp.com/api',
-    TOKEN_KEY: 'post-up-app-client-token',
-    JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'https://sleepy-fjord-75293.herokuapp.com/api',
+    TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'post-up-app-client-token',
+    JWT_SECRET: process.env.REACT_APP_JWT_SECRET || 'change-this-secret',
 }
